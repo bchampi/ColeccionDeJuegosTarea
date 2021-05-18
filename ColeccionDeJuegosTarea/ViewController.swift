99a5a5c2d05a5bde300510e729facd87ea439f4d
Mac 17 <brayan.champi@tecsup.edu.pe>
2021-05-18 11:59:35 -0500

    Initial Commit

---
+++ ColeccionDeJuegosTarea/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColeccionDeJuegosTarea
+//
+//  Created by Mac 17 on 5/18/21.
+//  Copyright © 2021 deah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
